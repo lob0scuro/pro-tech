a85a8c1db300d4a6ddcf776ad5044e08f7311189
Cameron Lopez <kamrin717@gmail.com>
2025-07-15 00:50:34 -0500

    one more try

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,4 +1,10 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {};
 
+// export default nextConfig;
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export",
+};
 export default nextConfig;
